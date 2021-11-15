de96e3a3d05b535b93da1151d1bc2236238b8cf8
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:16:47 +0200

    Initialize fix-get-block-difficulty-validation

---
+++ test/integration/config.js
@@ -0,0 +1,23 @@
+export default {
+  bitcoin: {
+    rpc: {
+      host: 'http://localhost:18332',
+      username: 'bitcoin',
+      password: 'local321'
+    },
+    network: 'bitcoin_regtest',
+    value: 1000000,
+    mineBlocks: true
+  },
+  ethereum: {
+    rpc: {
+      host: 'http://localhost:8545'
+    },
+    value: 10000000000000000,
+    network: 'local',
+    metaMaskConnector: {
+      port: 3333
+    }
+  },
+  timeout: 120000 // No timeout
+}

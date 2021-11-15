d50a470498d3e1b23b68c417c82abf523e0b3465
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:19:53 +0200

    Initialize fix/ethereum-scraper-swap-find-provider-normalize-tx-response

---
+++ packages/bitcoin-esplora-api-provider/lib/index.ts
@@ -0,0 +1,5 @@
+import BitcoinEsploraApiProvider, { EsploraApiProviderOptions } from './BitcoinEsploraApiProvider'
+import * as esplora from './types'
+
+export { esplora, EsploraApiProviderOptions }
+export default BitcoinEsploraApiProvider

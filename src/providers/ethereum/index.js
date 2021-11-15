0a356d42ceca1687d3b951d20dcd1ff0ac8c398b
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:16:16 +0200

    Initialize fix-address-retrieval

---
+++ src/providers/ethereum/index.js
@@ -0,0 +1,13 @@
+import EthereumRPCProvider from './EthereumRPCProvider'
+import EthereumLedgerProvider from './EthereumLedgerProvider'
+import EthereumMetaMaskProvider from './EthereumMetaMaskProvider'
+import EthereumSwapProvider from './EthereumSwapProvider'
+import networks from './networks'
+
+export {
+  EthereumRPCProvider,
+  EthereumLedgerProvider,
+  EthereumMetaMaskProvider,
+  EthereumSwapProvider,
+  networks
+}

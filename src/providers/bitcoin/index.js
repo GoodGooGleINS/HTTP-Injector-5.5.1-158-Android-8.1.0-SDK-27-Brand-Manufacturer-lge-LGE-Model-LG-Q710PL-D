bc5cfa05ffe2c2cb048178dbd8f32d64f08c35a8
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:15:52 +0200

    Initialize bitcore-pending

---
+++ src/providers/bitcoin/index.js
@@ -0,0 +1,15 @@
+import BitcoinRPCProvider from './BitcoinRPCProvider'
+import BitcoinLedgerProvider from './BitcoinLedgerProvider'
+import BitcoinBlockchainAPIProvider from './BitcoinBlockchainAPIProvider'
+import BitcoinSwapProvider from './BitcoinSwapProvider'
+import BitcoinJsLibSwapProvider from './BitcoinJsLibSwapProvider.js'
+import * as BitcoinUtil from './BitcoinUtil'
+
+export {
+  BitcoinRPCProvider,
+  BitcoinLedgerProvider,
+  BitcoinBlockchainAPIProvider,
+  BitcoinSwapProvider,
+  BitcoinJsLibSwapProvider,
+  BitcoinUtil
+}
---
+++ src/providers/index.js
@@ -0,0 +1,9 @@
+import * as bitcoin from './bitcoin'
+import * as ethereum from './ethereum'
+import * as bitcore from './bitcore'
+
+export {
+  bitcoin,
+  ethereum,
+  bitcore
+}

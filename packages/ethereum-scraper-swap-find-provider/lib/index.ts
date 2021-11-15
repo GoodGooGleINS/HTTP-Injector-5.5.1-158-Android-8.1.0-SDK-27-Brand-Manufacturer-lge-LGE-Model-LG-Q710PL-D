eb2aa928a0e91fa4453fddc0712b5806c30c0dc6
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:19:50 +0200

    Initialize dependabot/npm_and_yarn/ssri-6.0.2

---
+++ packages/ethereum-scraper-swap-find-provider/lib/index.ts
@@ -0,0 +1,5 @@
+import EthereumScraperSwapFindProvider from './EthereumScraperSwapFindProvider'
+import * as scraper from './types'
+
+export default EthereumScraperSwapFindProvider
+export { scraper }

2a3652ef76f13f59bb2b4a89dccce9db3f4d3da0
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:15:28 +0200

    Initialize getBalance-ethereum

---
+++ src/providers/ethereum/EthereumRPCProvider.js
@@ -0,0 +1,36 @@
+import JsonRpcProvider from '../JsonRpcProvider'
+
+import { formatEthResponse, ensureEthFormat } from './EthereumUtil'
+
+export default class EthereumRPCProvider extends JsonRpcProvider {
+  _parseResponse (response) {
+    const data = super._parseResponse(response)
+
+    return formatEthResponse(data)
+  }
+
+  async getAddresses () {
+    return this.rpc('eth_accounts')
+  }
+
+  async generateBlock (numberOfBlocks) {
+    // Q: throw or silently pass?
+    throw new Error('This method isn\'t supported by Ethereum')
+  }
+
+  async getBlockByNumber (blockNumber, includeTx) {
+    return this.rpc('eth_getBlockByNumber', blockNumber, includeTx)
+  }
+
+  async getTransactionByHash (txHash) {
+    txHash = ensureEthFormat(txHash)
+    return this.rpc('eth_getTransactionByHash', txHash)
+  }
+
+  async getBalance (addresses) {
+    const addrs = addresses.map(ensureEthFormat)
+    const promiseBalances = await Promise.all(addrs.map(address => this.rpc('eth_getBalance', address, 'latest')))
+    return promiseBalances.map(balance => parseInt(balance, 16))
+      .reduce((acc, balance) => acc + balance, 0)
+  }
+}

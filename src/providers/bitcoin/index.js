b9e85ef6c247ed889f61eccfb079f102b8008407
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:16:28 +0200

    Initialize eth-fixes

---
+++ src/providers/bitcoin/index.js
@@ -0,0 +1,17 @@
+import BitcoinRPCProvider from './BitcoinRPCProvider'
+import BitcoreRPCProvider from './BitcoreRPCProvider'
+import BitcoinLedgerProvider from './BitcoinLedgerProvider'
+import BitcoinSwapProvider from './BitcoinSwapProvider'
+import BitcoinJsLibSwapProvider from './BitcoinJsLibSwapProvider.js'
+import * as BitcoinUtil from './BitcoinUtil'
+import networks from './networks'
+
+export {
+  BitcoinRPCProvider,
+  BitcoreRPCProvider,
+  BitcoinLedgerProvider,
+  BitcoinSwapProvider,
+  BitcoinJsLibSwapProvider,
+  BitcoinUtil,
+  networks
+}

a6852b8dc6a7365facf2ada32d03cdadab4e659b
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:16:47 +0200

    Initialize implement-bitcoin-js-wallet-provider-wip

---
+++ packages/bundle/lib/providers.js
@@ -0,0 +1,46 @@
+import BitcoinLedgerProvider from '@liquality/bitcoin-ledger-provider'
+import BitcoinRpcProvider from '@liquality/bitcoin-rpc-provider'
+import BitcoinNodeWalletProvider from '@liquality/bitcoin-node-wallet-provider'
+import BitcoinJsWalletProvider from '@liquality/bitcoin-js-wallet-provider'
+import BitcoinSwapProvider from '@liquality/bitcoin-swap-provider'
+import BitcoinEsploraApiProvider from '@liquality/bitcoin-esplora-api-provider'
+import * as BitcoinNetworks from '@liquality/bitcoin-networks'
+import * as BitcoinUtils from '@liquality/bitcoin-utils'
+
+import EthereumErc20Provider from '@liquality/ethereum-erc20-provider'
+import EthereumErc20SwapProvider from '@liquality/ethereum-erc20-swap-provider'
+import EthereumLedgerProvider from '@liquality/ethereum-ledger-provider'
+import EthereumMetaMaskProvider from '@liquality/ethereum-metamask-provider'
+import EthereumRpcProvider from '@liquality/ethereum-rpc-provider'
+import EthereumSwapProvider from '@liquality/ethereum-swap-provider'
+import * as EthereumNetworks from '@liquality/ethereum-networks'
+import * as EthereumUtils from '@liquality/ethereum-utils'
+
+const bitcoin = {
+  BitcoinLedgerProvider,
+  BitcoinRpcProvider,
+  BitcoinNodeWalletProvider,
+  BitcoinJsWalletProvider,
+  BitcoinSwapProvider,
+  BitcoinEsploraApiProvider,
+  BitcoinNetworks,
+  BitcoinUtils,
+  networks: BitcoinNetworks
+}
+
+const ethereum = {
+  EthereumErc20Provider,
+  EthereumErc20SwapProvider,
+  EthereumLedgerProvider,
+  EthereumMetaMaskProvider,
+  EthereumRpcProvider,
+  EthereumSwapProvider,
+  EthereumNetworks,
+  EthereumUtils,
+  networks: EthereumNetworks
+}
+
+export {
+  bitcoin,
+  ethereum
+}

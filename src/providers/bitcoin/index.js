e44efa2554ddfe75029d1716a747b78a4a0b7662
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:15:51 +0200

    Initialize fix-tx-objects

---
+++ src/providers/bitcoin/index.js
@@ -0,0 +1,15 @@
+import BitcoinRPCProvider from './BitcoinRPCProvider'
+import BitcoreRPCProvider from './BitcoreRPCProvider'
+import BitcoinLedgerProvider from './BitcoinLedgerProvider'
+import BitcoinBlockchainAPIProvider from './BitcoinBlockchainAPIProvider'
+import BitcoinSwapProvider from './BitcoinSwapProvider'
+import * as BitcoinUtil from './BitcoinUtil'
+
+export {
+  BitcoinRPCProvider,
+  BitcoreRPCProvider,
+  BitcoinLedgerProvider,
+  BitcoinBlockchainAPIProvider,
+  BitcoinSwapProvider,
+  BitcoinUtil
+}
---
+++ test/methods.js
@@ -0,0 +1,33 @@
+/* global describe, it */
+
+const { expect } = require('chai').use(require('chai-as-promised'))
+
+const { Client, errors } = require('../')
+
+const lib = new Client()
+
+describe('Client methods', () => {
+  describe('generateBlock', () => {
+    it('should throw NoProviderError', () => {
+      return expect(lib.generateBlock(1)).to.be.rejectedWith(errors.NoProviderError)
+    })
+  })
+
+  describe('getBlockByNumber', () => {
+    it('should throw NoProviderError', async () => {
+      return expect(lib.getBlockByNumber(1)).to.be.rejectedWith(errors.NoProviderError)
+    })
+  })
+
+  describe('getBlockHeight', () => {
+    it('should throw NoProviderError', async () => {
+      return expect(lib.getBlockHeight()).to.be.rejectedWith(errors.NoProviderError)
+    })
+  })
+
+  describe('getTransactionByHash', () => {
+    it('should throw NoProviderError', async () => {
+      return expect(lib.getTransactionByHash('4545')).to.be.rejectedWith(errors.NoProviderError)
+    })
+  })
+})

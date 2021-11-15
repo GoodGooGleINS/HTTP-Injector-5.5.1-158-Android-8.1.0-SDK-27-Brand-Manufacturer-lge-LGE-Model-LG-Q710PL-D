f47ec5afab22d499420ead72e4cd55a10a9c5095
GoodGooGleINS <86702837+GoodGooGleINS@users.noreply.github.com>
2021-11-15 03:16:17 +0200

    Initialize node-error

---
+++ src/providers/ethereum/EthereumLedgerProvider.js
@@ -0,0 +1,24 @@
+import Ethereum from '@ledgerhq/hw-app-eth'
+
+import LedgerProvider from '../LedgerProvider'
+import networks from './networks'
+import Address from '../../Address'
+
+export default class EthereumLedgerProvider extends LedgerProvider {
+  constructor (chain = { network: networks.mainnet }) {
+    super(Ethereum, `44'/${chain.network.coinType}'/0'/`)
+  }
+
+  async getAddressFromDerivationPath (path) {
+    const app = await this.getApp()
+    const { address } = await app.getAddress(path)
+    return new Address(address, path)
+  }
+
+  async signMessage (message, from) {
+    const app = await this.getApp()
+    const address = await this.getWalletAddress(from)
+    const hex = Buffer.from(message).toString('hex')
+    return app.signPersonalMessage(address.derivationPath, hex)
+  }
+}
